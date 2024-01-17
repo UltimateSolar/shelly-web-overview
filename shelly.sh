da6dce141ec6dd433d1bed5dc109e63120b90fd1
UltimateSolar <nobody@cares.com>
2024-01-17 12:52:02 +0100

    improvements

--- shelly.sh
+++ shelly.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# shelly data poller v1.0 (2024-01)
-# define a cron job that will run every 10min */10  * * * * /scripts/shelly.sh
+# shelly data poller v1.1 (2024-01)
+# define a cron job that will run every 1min * * * * * /scripts/shelly.sh
 # tested with: plug s, firmware 1.1
 
 # === config start ===
+
+# it is HIGHLY recommended to set the same password at all shellies
+PASSWORD=1234
 
 # list of shellies (fixed ip address would be good for reliability)
 declare -A shelly
@@ -14,11 +17,6 @@ shelly[2]=192.168.0.136
 
 # where the data should be stored (note that there is NO / at the end)
 data=/var/www/html/shelly/data
-
-# it is assumed all shellies are configured with the same password
-# shellies can not do HTTPS so please use a password that is not used anywhere else, as it is send in clear text over the network in use
-PASSWORD=(auth currently does not work)
-
 # === config end ===
 
 # === start polling ===
@@ -27,10 +25,15 @@ do
 	IP=${shelly[$key]}
 	LOGFILENAME="$data/shelly.$(date '+%Y-%m-%d').log"
 	printf "\n$(date '+%Y-%m-%d===%H:%M:%S')===PollingShelly===$IP===" >> $LOGFILENAME
-
+	# query the device
+    	curl -s --tlsv1.2 --digest -u admin:$PASSWORD http://$IP/rpc/Shelly.GetStatus >> $LOGFILENAME
 	# curl -s --tlsv1.2 http://$IP/rpc/Shelly.GetDeviceInfo
-	curl -s --tlsv1.2 http://$IP/rpc/Shelly.GetStatus >> $LOGFILENAME
+	# curl -s --tlsv1.2 http://$IP/rpc/Shelly.GetStatus >> $LOGFILENAME
 done
+
+# options:
+# -s
+# silent, do not output curl progress
 
 # change ownership so that apache2+php are allowed read access
 # chown -R www-data: $data
